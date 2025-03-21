156a7fd87cd4d4da1cf38b0c49b14b3628ed11b4
Majok-Bol <bolmajok001@gmail.com>
2025-03-21 22:10:16 +0300

    reduced circle radius

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,30 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        // backgroundColor: Colors.indigo,
         appBar: AppBar(title: Text('Image Widget',style: TextStyle(color: Colors.white,fontSize: 20),),centerTitle: true,backgroundColor: Colors.purple,),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.symmetric(vertical: 50,horizontal: 50),
-          child: Column(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Container(
+    padding: EdgeInsets.all(10),
+            margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+            child: Column(
             children: [
+              CircleAvatar(radius: 40,backgroundColor: Colors.lightBlueAccent,),
+              SizedBox(height: 10,),
+              Text('Majok Bol',style: TextStyle(color: Colors.black87,fontSize: 20,fontWeight: FontWeight.bold),),
+              SizedBox(height: 5,),
+              Text('Android | IOS Developer',style: TextStyle(color: Colors.black87,fontWeight: FontWeight.bold,fontSize: 20),),
+              SizedBox(height: 30,),
               Row(
                 children: [
-                  Expanded(child: Image.asset('assets/images/p1.jpg'),),
+                  Flexible(child: Image.asset('assets/images/p1.jpg'),),
                   SizedBox(width: 20,),
-                  Expanded(child: Image.asset('assets/images/p2.jpg'),),
+                  Flexible(child: Image.asset('assets/images/p2.jpg'),),
                   SizedBox(width: 20,),
-                  Expanded(child: Image.asset('assets/images/p3.jpeg'),),
+                  Flexible(child: Image.asset('assets/images/p3.jpeg'),),
                   SizedBox(width: 20,),
-                  Expanded(child: Image.asset('assets/images/p4.jpeg'),),
+                  Flexible(child: Image.asset('assets/images/p4.jpeg'),),
                 ],
               ),
               SizedBox(height: 20,),
@@ -41,13 +50,14 @@ class MyApp extends StatelessWidget{
               SizedBox(height: 20,),
               Row(
                 children: [
-                  Text('Everything in Flutter is a Widget like the images above',style: TextStyle(fontSize: 20,),),
+                  Expanded(child: Text('Everything in Flutter is a Widget like the images above',style: TextStyle(fontSize: 20,),),),
                 ],
               ),
 
 
 
             ],
+          ),
           ),
         ),
       ),
