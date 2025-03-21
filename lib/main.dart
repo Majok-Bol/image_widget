e26379f88377958e8b34b951071619133d28a39d
Majok-Bol <bolmajok001@gmail.com>
2025-03-21 15:46:05 +0300

    added more images

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,36 @@ class MyApp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(title: Text('Image Widget',style: TextStyle(color: Colors.white,fontSize: 20),),centerTitle: true,backgroundColor: Colors.purple,),
-        backgroundColor: Colors.grey,
         body: Container(
           padding: EdgeInsets.all(10),
           margin: EdgeInsets.symmetric(vertical: 50,horizontal: 50),
           child: Column(
             children: [
-           Expanded(child: Image.asset('assets/images/p1.jpg'),),
-              Expanded(child: Image.asset('assets/images/p2.jpg'),),
-              Expanded(child: Image.asset('assets/images/p3.jpeg'),),
-              Expanded(child: Image.asset('assets/images/p4.jpeg'),),
+              Row(
+                children: [
+                  Expanded(child: Image.asset('assets/images/p1.jpg'),),
+                  SizedBox(width: 20,),
+                  Expanded(child: Image.asset('assets/images/p2.jpg'),),
+                  SizedBox(width: 20,),
+                  Expanded(child: Image.asset('assets/images/p3.jpeg'),),
+                  SizedBox(width: 20,),
+                  Expanded(child: Image.asset('assets/images/p4.jpeg'),),
+                ],
+              ),
+              SizedBox(height: 20,),
+              Row(
+                children: [
+                  Expanded(child: Image.asset('assets/images/p5.jpeg'),),
+                  SizedBox(width: 20,),
+                  Expanded(child: Image.asset('assets/images/p6.jpeg'),),
+                  SizedBox(width: 20,),
+                  Expanded(child: Image.asset('assets/images/p7.jpeg'),),
+                  SizedBox(width: 20,),
+                  Expanded(child: Image.asset('assets/images/p8.jpeg'),),
+                ],
+              ),
+
+
 
             ],
           ),
